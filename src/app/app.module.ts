d6984836a2ebfd6cdab6b0aa24868385c30089cd
StevenSevillanoR <31101311+StevenSevillanoR@users.noreply.github.com>
2018-08-16 00:50:30 +0200

    Instalando Bootstrap y otras dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CarritoComprasComponent } from './carrito-compras/carrito-compras.component';
+import { DescripProductoComponent } from './descrip-producto/descrip-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomePageComponent,
+    CarritoComprasComponent,
+    DescripProductoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
